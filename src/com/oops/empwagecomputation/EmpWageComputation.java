91b6def643e43d1282277d62c04a57c6e02ead82
am22ar <amarpatil12222@gmail.com>
2022-09-30 20:25:31 +0530

    Added: Employee Daily wage calculation Logic

--- src/com/oops/empwagecomputation/EmpWageComputation.java
+++ src/com/oops/empwagecomputation/EmpWageComputation.java
@@ -3,15 +3,20 @@ import java.util.*;
 public class EmpWageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program using OOPS Concept");
+		int empWagePerHour = 20;
+		int fullDayHour = 8;
+		int salary = 0;
 		Random random = new Random();
 		int empCheck = random.nextInt(2);
 		if(empCheck == 1)
 		{
 			System.out.println("Employee is Present.");
+			salary = empWagePerHour * fullDayHour ;
 		}
 		else
 		{
 			System.out.println("Employee is Absent.");
 		}
+		System.out.println("Salary is: "+salary);
 	}
 }
