5cf7e488f0cebfe2d0cf17328166725ed52f200b
am22ar <amarpatil12222@gmail.com>
2022-09-30 20:22:43 +0530

    Added: Employee Present/Absent Logic

--- src/com/oops/empwagecomputation/EmpWageComputation.java
+++ src/com/oops/empwagecomputation/EmpWageComputation.java
@@ -1,7 +1,17 @@
 package com.oops.empwagecomputation;
-
+import java.util.*;
 public class EmpWageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program using OOPS Concept");
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		if(empCheck == 1)
+		{
+			System.out.println("Employee is Present.");
+		}
+		else
+		{
+			System.out.println("Employee is Absent.");
+		}
 	}
 }
